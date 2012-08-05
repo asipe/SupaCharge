16dbfa28a3f79d9354d69b554e2f06b22167bf95
Andy Sipe <ajs.general@gmail.com>
2012-08-05 10:45:42 -0400

    initial project setup

---
+++ scripts/setpaths.bat
@@ -0,0 +1,1 @@
+set PATH=%PATH%;thirdparty\nunit\bin;thirdparty\nant\bin
---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SupaCharge.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Z Bar Technologies, LLC")]
+[assembly: AssemblyProduct("SupaCharge.Core")]
+[assembly: AssemblyCopyright("(c) 2012 Morgan Sipe (morgan.talk@gmail.com), Andy Sipe (ajs.general@gmail.com)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("63965a07-5af7-4daa-9d8e-fc2f9db22842")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
