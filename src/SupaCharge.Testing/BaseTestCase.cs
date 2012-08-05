ec00cb9d47b820799acbdc4f923fa9b83fb475d1
Andy Sipe <ajs.general@gmail.com>
2012-08-05 10:59:02 -0400

    added testing project and BaseTestCase impl

---
+++ src/SupaCharge.Testing/BaseTestCase.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using Moq;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace SupaCharge.Testing {
+  public abstract class BaseTestCase {
+    protected Fixture ObjectFixture{get;private set;}
+    protected MockFactory MokFac{get;private set;}
+
+    [SetUp]
+    public void BaseSetup() {
+      MokFac = new MockFactory(MockBehavior.Strict);
+      ObjectFixture = new Fixture();
+    }
+
+    [TearDown]
+    public void BaseTearDown() {
+      if (TestContext.CurrentContext.Result.Status == TestStatus.Passed)
+        MokFac.VerifyAll();
+    }
+
+    protected Mock<T> Mok<T>() where T : class {
+      return MokFac.Create<T>();
+    }
+
+    protected T[] CM<T>() {
+      return CM<T>(3);
+    }
+
+    protected T[] CM<T>(int number) {
+      return ObjectFixture.CreateMany<T>(number).ToArray();
+    }
+
+    protected T CA<T>() {
+      return ObjectFixture.CreateAnonymous<T>();
+    }
+
+    protected T[] BA<T>(params T[] objects) {
+      return objects;
+    }
+  }
+}
