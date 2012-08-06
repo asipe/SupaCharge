82aa1f49d7f79266dd44aab81a6d06b14f606d72
Morgan Sipe <morgan.talk@gmail.com>
2012-08-06 14:49:25 -0400

    got the toString method to work and test to pass

---
+++ src/SupaCharge.Core/ExceptionHandling/ExceptionDump.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SupaCharge.Core.ExceptionHandling {
+  public class ExceptionDump {
+    public ExceptionDump(Exception error) {
+      mError = error;
+      ToString();
+    }
+
+    public override string ToString() {
+      return mError.Message;
+    }
+
+    private Exception mError;
+  }
+}
---
+++ src/SupaCharge.UnitTests/Core/ExceptionHandling/ExceptionDumpTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SupaCharge.Testing;
+using NUnit.Framework;
+using SupaCharge.Core.ExceptionHandling;
+
+namespace SupaCharge.UnitTests.Core.ExceptionHandling {
+  [TestFixture]
+  public class ExceptionDumpTest : BaseTestCase {
+    [Test]
+    public void TestDumpsMessage() {
+      var dump = new ExceptionDump(new Exception("hello world"));
+      Assert.That(dump.ToString(), Is.EqualTo("hello world"));
+    }
+
+  }
+}
