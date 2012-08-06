a8cc2cf0dc428eac2d744f16f9bab718679ec207
Morgan Sipe <morgan.talk@gmail.com>
2012-08-06 14:12:54 -0400

    Set up the project's test files and got it ready to work

---
+++ src/SupaCharge.UnitTests/Core/ExceptionHandling/ExceptionDumperTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SupaCharge.Testing;
+using NUnit.Framework;
+
+namespace SupaCharge.UnitTests.Core.ExceptionHandling {
+  [TestFixture]
+  public class ExceptionDumperTest : BaseTestCase {
+    
+  
+  }
+}
