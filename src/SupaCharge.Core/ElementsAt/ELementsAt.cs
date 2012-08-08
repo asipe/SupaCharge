d2f4d5286abcb7a28b0cde5d335227f197fe9462
Morgan Sipe <morgan.talk@gmail.com>
2012-08-08 14:46:14 -0400

    Set up elements at method

---
+++ src/SupaCharge.Core/ElementsAt/ELementsAt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SupaCharge.Core.ElementsAt {
+  public static class ELementsAtExtension {
+    public static T[] ElementsAt<T>(this IEnumerable<T> items, params int[] indexes) {
+       List<T> Result = new List<T>();
+      
+      for (var x = 0; x < indexes.Length; x++) {
+        Result.Add(items.ElementAt(x));
+      }
+
+      return Result.ToArray() ;
+    }
+  }
+}
---
+++ src/SupaCharge.UnitTests/Core/ElementsAt/ElementsAtTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using SupaCharge.Core.ElementsAt;
+using SupaCharge.Testing;
+
+
+namespace SupaCharge.UnitTests.Core.ElementsAt {
+  [TestFixture]
+  public class ElementsAtTestcs:BaseTestCase {
+    [Test]
+    public void TestElementsAt() {
+      var ary = new[] { "a", "b", "c" };
+
+      Assert.That(ary.ElementsAt(), Is.EqualTo(new string[0]));
+      Assert.That(ary.ElementsAt(0), Is.EqualTo(new [] {"a"}));
+      Assert.That(ary.ElementsAt(0, 1), Is.EqualTo(new[] { "a", "b" }));
+    }
+
+  }
+}
