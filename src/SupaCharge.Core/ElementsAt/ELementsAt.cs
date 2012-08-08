1dce899e948da738a8959b4e6b19761096170002
Morgan Sipe <morgan.talk@gmail.com>
2012-08-08 14:58:24 -0400

    updated some of the src to be able to handle more challenging scenarios

--- src/SupaCharge.Core/ElementsAt/ELementsAt.cs
+++ src/SupaCharge.Core/ElementsAt/ELementsAt.cs
@@ -6,10 +6,10 @@ using System.Text;
 namespace SupaCharge.Core.ElementsAt {
   public static class ELementsAtExtension {
     public static T[] ElementsAt<T>(this IEnumerable<T> items, params int[] indexes) {
-       List<T> Result = new List<T>();
+      List<T> Result = new List<T>();
       
       for (var x = 0; x < indexes.Length; x++) {
-        Result.Add(items.ElementAt(x));
+        Result.Add(items.ElementAt(indexes[x]));
       }
 
       return Result.ToArray() ;
--- src/SupaCharge.UnitTests/Core/ElementsAt/ElementsAtTest.cs
+++ src/SupaCharge.UnitTests/Core/ElementsAt/ElementsAtTest.cs
@@ -17,7 +17,9 @@ namespace SupaCharge.UnitTests.Core.ElementsAt {
       Assert.That(ary.ElementsAt(), Is.EqualTo(new string[0]));
       Assert.That(ary.ElementsAt(0), Is.EqualTo(new [] {"a"}));
       Assert.That(ary.ElementsAt(0, 1), Is.EqualTo(new[] { "a", "b" }));
+      Assert.That(ary.ElementsAt(0, 1, 2), Is.EqualTo(new[] { "a", "b", "c" }));
+      Assert.That(ary.ElementsAt(0, 1, 0), Is.EqualTo(new[] { "a", "b", "a" }));
+      Assert.That(ary.ElementsAt(0, 1, 0, 0, 1, 0, 1), Is.EqualTo(new[] { "a", "b", "a", "a", "b", "a", "b" }));
     }
-
   }
 }
