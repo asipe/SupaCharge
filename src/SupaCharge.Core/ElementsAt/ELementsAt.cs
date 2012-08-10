1ec31823c6c66c209abcd2e036f522692328fd90
Andy Sipe <ajs.general@gmail.com>
2012-08-10 09:46:42 -0400

    fixed class naming

--- src/SupaCharge.Core/ElementsAt/ELementsAt.cs
+++ src/SupaCharge.Core/ElementsAt/ELementsAt.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 
 namespace SupaCharge.Core.ElementsAt {
-  public static class ELementsAtExtension {
+  public static class ElementsAtExtension {
     public static IEnumerable<T> ElementsAt<T>(this IEnumerable<T> items, params int[] indexes) {
       return indexes.Select(x => items.ElementAt(x));
     }
