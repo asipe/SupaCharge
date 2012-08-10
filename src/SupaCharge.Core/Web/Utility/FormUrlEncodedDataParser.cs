1938ddfbe4f98c87cb2e09d954cc0782fedca939
Andy Sipe <ajs.general@gmail.com>
2012-08-10 12:22:13 -0400

    added FormURLEncodedDataParser

---
+++ src/SupaCharge.Core/Web/Utility/FormUrlEncodedDataParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SupaCharge.Core.Web.Utility {
+  public class FormUrlEncodedDataParser {
+    public IDictionary<string, string> Parse(string data) {
+      return IsNullOrEmptyTrim(data) 
+               ? new Dictionary<string, string>()
+               : ParseData(data);
+    }
+
+    private static bool IsNullOrEmptyTrim(string data) {
+      return string.IsNullOrEmpty(data) || string.IsNullOrEmpty(data.Trim());
+    }
+
+    private static IDictionary<string, string> ParseData(string data) {
+      return data
+        .Split('&')
+        .Select(s => s.Split('='))
+        .ToDictionary(s => s[0], s => Uri.UnescapeDataString(s[1]));
+    }
+  }
+}
---
+++ src/SupaCharge.UnitTests/Web/Utility/FormUrlEncodedDataParserTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using SupaCharge.Core.Web.Utility;
+using SupaCharge.Testing;
+
+namespace SupaCharge.UnitTests.Web.Utility {
+  [TestFixture]
+  public class FormUrlEncodedDataParserTest : BaseTestCase {
+    [TestCaseSource("GetParsingTests")]
+    public void TestParsing(string data, Dictionary<string, string> expected) {
+      var actual = mParser.Parse(data);
+      Assert.That(actual.Count, Is.EqualTo(expected.Count));
+      foreach (var key in expected.Keys)
+        Assert.That(expected[key], Is.EqualTo(actual[key]));
+    }
+
+    public IEnumerable GetParsingTests() {
+      yield return new TestCaseData("key1=value1", new Dictionary<string, string> {{"key1", "value1"}}).SetName("SingleValue");
+      yield return new TestCaseData("key1=value1&key2=value2", new Dictionary<string, string> {{"key1", "value1"}, {"key2", "value2"}}).SetName("MultipleValues");
+      yield return new TestCaseData("key1=value 1&key2= value2 ", new Dictionary<string, string> {{"key1", "value 1"}, {"key2", " value2 "}}).SetName("MultipleValuesWithWhiteSpace");
+      yield return new TestCaseData("key1=&key2=", new Dictionary<string, string> {{"key1", ""}, {"key2", ""}}).SetName("MissingValues");
+
+      var data = string.Format("key1={0}&key2=value2", Uri.EscapeDataString("<h\\el/lo= & goodbye=>"));
+      yield return new TestCaseData(data, new Dictionary<string, string> {{"key1", "<h\\el/lo= & goodbye=>"}, {"key2", "value2"}}).SetName("EncodedValue");
+      yield return new TestCaseData("", new Dictionary<string, string>()).SetName("EmptyValue");
+      yield return new TestCaseData("       ", new Dictionary<string, string>()).SetName("SpacesOnlyValue");
+      yield return new TestCaseData(null, new Dictionary<string, string>()).SetName("NullValue");
+    }
+
+    private readonly FormUrlEncodedDataParser mParser = new FormUrlEncodedDataParser();
+  }
+}
