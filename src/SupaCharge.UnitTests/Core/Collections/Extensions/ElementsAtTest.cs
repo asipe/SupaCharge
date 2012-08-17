e045af3c31809f9e0de4f01a3798ef39383ef2f8
Morgan Sipe <morgan.talk@gmail.com>
2012-08-17 08:27:57 -0400

    removed redundant test case

--- src/SupaCharge.UnitTests/Core/Collections/Extensions/ElementsAtTest.cs
+++ src/SupaCharge.UnitTests/Core/Collections/Extensions/ElementsAtTest.cs
@@ -8,18 +8,6 @@ using System.Linq;
 namespace SupaCharge.UnitTests.Core.Collections.Extensions {
   [TestFixture]
   public class ElementsAtTest: BaseTestCase {
-    [Test]
-    public void TestElementsAt() {
-      var ary = new[] { "a", "b", "c" };
-
-      Assert.That(ary.ElementsAt(), Is.EqualTo(new string[0]));
-      Assert.That(ary.ElementsAt(0), Is.EqualTo(new[] { "a" }));
-      Assert.That(ary.ElementsAt(0, 1), Is.EqualTo(new[] { "a", "b" }));
-      Assert.That(ary.ElementsAt(0, 1, 2), Is.EqualTo(new[] { "a", "b", "c" }));
-      Assert.That(ary.ElementsAt(0, 1, 0), Is.EqualTo(new[] { "a", "b", "a" }));
-      Assert.That(ary.ElementsAt(0, 1, 0, 0, 1, 0, 1), Is.EqualTo(new[] { "a", "b", "a", "a", "b", "a", "b" }));
-    }
-    
     [TestCase(new int[0], new string[0])]
     [TestCase(new [] {0}, new [] {"a"})]
     [TestCase(new[] { 0, 0 }, new[] { "a", "a" })]
