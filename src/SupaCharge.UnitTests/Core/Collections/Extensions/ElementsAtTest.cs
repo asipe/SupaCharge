9c742bcc35c085a9ab11cdb8406659b16afd9007
Morgan Sipe <morgan.talk@gmail.com>
2012-08-17 08:27:05 -0400

    updated the Elements at test to be in test table format

--- src/SupaCharge.UnitTests/Core/Collections/Extensions/ElementsAtTest.cs
+++ src/SupaCharge.UnitTests/Core/Collections/Extensions/ElementsAtTest.cs
@@ -19,6 +19,18 @@ namespace SupaCharge.UnitTests.Core.Collections.Extensions {
       Assert.That(ary.ElementsAt(0, 1, 0), Is.EqualTo(new[] { "a", "b", "a" }));
       Assert.That(ary.ElementsAt(0, 1, 0, 0, 1, 0, 1), Is.EqualTo(new[] { "a", "b", "a", "a", "b", "a", "b" }));
     }
+    
+    [TestCase(new int[0], new string[0])]
+    [TestCase(new [] {0}, new [] {"a"})]
+    [TestCase(new[] { 0, 0 }, new[] { "a", "a" })]
+    [TestCase(new[] { 0, 1, 2}, new[] { "a", "b", "c" })]
+    [TestCase(new[] { 0, 1, 0 }, new[] { "a", "b", "a" })]
+    [TestCase(new[] { 0, 1, 0, 0, 1, 0, 1 }, new[] { "a", "b", "a", "a", "b", "a", "b" })]
+
+    public void TestElementsAt (int[] indexes, string[] expected) {
+      var ary = new[] { "a", "b", "c" };
+      Assert.That(ary.ElementsAt(indexes), Is.EqualTo(expected));
+    }
 
    [Test]
    public void TestIfExceptionIsThrown2() {
