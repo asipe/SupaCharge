3d4195c918ed19bdd5d9cf91769885ee3e8b83f6
Morgan Sipe <morgan.talk@gmail.com>
2012-08-27 10:21:50 -0400

    set up initial tests and code for the value converter

---
+++ src/SupaCharge.UnitTests/Core/Converter/ValueConverterTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SupaCharge.Testing;
+using NUnit.Framework;
+using SupaCharge.Core.Converter;
+
+namespace SupaCharge.UnitTests.Core.Converter {
+  [TestFixture]
+  public class ValueConverterTest:BaseTestCase {
+    [Test]
+    public void TestBasicConversion() {
+      var converter = new ValueConverter();
+      Assert.That(converter.Get<int>("3"), Is.EqualTo(3));
+      Assert.That(converter.Get<int>("yo"), Is.EqualTo(3));
+    }
+    
+  }
+}
