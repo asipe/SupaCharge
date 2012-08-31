68c95f4bf5419c0e31be9517206ce7edd7f5c6ce
Morgan Sipe <morgan.talk@gmail.com>
2012-08-31 08:49:07 -0400

    cleaned up the file

--- src/SupaCharge.Core/ConfigurationWrapper/ConfigurationWrapper.cs
+++ src/SupaCharge.Core/ConfigurationWrapper/ConfigurationWrapper.cs
@@ -8,25 +8,21 @@ using SupaCharge.Core.Converter;
 namespace SupaCharge.Core.ConfigurationWrapper {
   public class ConfigurationWrapper {
     public bool Contains(string key) {
-
-      if (ConfigurationSettings.AppSettings[key] != null) {
+      if (ConfigurationSettings.AppSettings[key] != null) 
         return true;
-      }
-
       return false;
     }
 
     public T Get<T>(string key) {
       var c = new ValueConverter();
-
       return c.Get<T>(ConfigurationSettings.AppSettings[key]);
     }
 
     public T Get<T>(string key, T defValue) {
       var c = new ValueConverter();
-      if (ConfigurationSettings.AppSettings[key] == null) {
+      
+      if (ConfigurationSettings.AppSettings[key] == null)
         return defValue;
-      }
       return c.Get<T>(ConfigurationSettings.AppSettings[key]);
     }
   }
