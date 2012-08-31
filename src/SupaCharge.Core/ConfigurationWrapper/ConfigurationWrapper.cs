5582d56270b39ac83b0b0a5d825994b65a79e64b
Morgan Sipe <morgan.talk@gmail.com>
2012-08-31 08:30:59 -0400

    got the config wrapper to work with basic vals and defvals

---
+++ src/SupaCharge.Core/ConfigurationWrapper/ConfigurationWrapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using SupaCharge.Core.Converter;
+
+namespace SupaCharge.Core.ConfigurationWrapper {
+  public class ConfigurationWrapper {
+    public bool Contains(string key) {
+
+      if (ConfigurationSettings.AppSettings[key].Length > 0) {
+        return true;
+      }
+
+      return false;
+    }
+
+    public T Get<T>(string key) {
+      var c = new ValueConverter();
+
+      return c.Get<T>(ConfigurationSettings.AppSettings[key]);
+    }
+
+    public T Get<T>(string key, T defValue) {
+      var c = new ValueConverter();
+      if (ConfigurationSettings.AppSettings[key] == null) {
+        return defValue;
+      }
+      return c.Get<T>(ConfigurationSettings.AppSettings[key]);
+    }
+  }
+}
---
+++ src/SupaCharge.UnitTests/Core/ConfigurationWrapper/ConfigWrapperTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SupaCharge.Core.ConfigurationWrapper;
+using NUnit.Framework;
+
+namespace SupaCharge.UnitTests.Core.ConfigWrapper {
+  [TestFixture]
+  class ConfigWrapperTest {
+    [Test]
+    public void BasicContains() {
+      var CWrapper = new ConfigurationWrapper();
+
+      Assert.That(CWrapper.Contains("name"), Is.True);
+      Assert.That(CWrapper.Contains("num"), Is.True);
+    }
+
+    [Test]
+    public void BasicGet() {
+      var CWrapper = new ConfigurationWrapper();
+
+      Assert.That(CWrapper.Get<string>("name"), Is.EqualTo("joe"));
+      Assert.That(CWrapper.Get<int>("num"), Is.EqualTo(123));
+    }
+
+    [Test]
+    public void BasicGetWithDefualtVals() {
+      var CWrapper = new ConfigurationWrapper();
+
+      Assert.That(CWrapper.Get<string>("nam", "harold"), Is.EqualTo("harold"));
+      Assert.That(CWrapper.Get<int>("nom-nom", 321), Is.EqualTo(321));
+    }
+  }
+}
