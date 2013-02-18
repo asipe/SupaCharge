b99c97035ff5a107afe92c3bf0a72f3acbac160d
Andy Sipe <ajs.general@gmail.com>
2013-02-18 09:17:16 -0500

    added IOIDProvider and guid based implementation

---
+++ src/SupaCharge.Core/OID/GuidOIDProvider.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SupaCharge.Core.OID {
+  public class GuidOIDProvider : IOIDProvider {
+    public string GetID() {
+      return Guid.NewGuid().ToString("N");
+    }
+  }
+}
---
+++ src/SupaCharge.UnitTests/Core/OID/GuidOIDProviderTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using SupaCharge.Core.OID;
+using SupaCharge.Testing;
+
+namespace SupaCharge.UnitTests.Core.OID {
+  [TestFixture]
+  public class GuidOIDProviderTest : BaseTestCase {
+    [Test]
+    public void TestGetID() {
+      var oid = new GuidOIDProvider().GetID();
+      Assert.That(oid.Length, Is.EqualTo(32));
+    }
+  }
+}
