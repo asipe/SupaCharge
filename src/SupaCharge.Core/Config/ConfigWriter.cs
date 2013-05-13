f3e1108bd3cc3f0e5694e78b05e9c85c7cdda2cb
Andy Sipe <ajs.general@gmail.com>
2013-05-13 11:25:11 -0400

    Added the initial implementation/tests for the config writer

---
+++ src/SupaCharge.Core/Config/ConfigWriter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using SupaCharge.Core.IOAbstractions;
+
+namespace SupaCharge.Core.Config {
+  public class ConfigWriter {
+    public ConfigWriter(IFile file, string filePath) {
+      mDoc = XDocument.Parse(file.ReadAllText(filePath));
+      mFile = file;
+      mPath = filePath;
+    }
+
+    public void Save() {
+      using (var writer = new StringWriter()) {
+        mDoc.Save(writer);
+        mFile.WriteAllText(mPath, writer.ToString());
+      }
+    }
+
+    public void Set(string nodeToChange, string newValue) {
+      var node = mDoc.XPathSelectElement("//configuration/appSettings/add");
+      node.Attribute(nodeToChange).Value = newValue;
+    }
+
+    private readonly XDocument mDoc;
+    private readonly IFile mFile;
+    private readonly string mPath;
+  }
+}
---
+++ src/SupaCharge.UnitTests/Core/Config/ConfigWriterTest.cs
@@ -0,0 +1,67 @@
+﻿using System.Xml.Linq;
+using System.Xml.XPath;
+using Moq;
+using NUnit.Framework;
+using SupaCharge.Core.Config;
+using SupaCharge.Core.IOAbstractions;
+using SupaCharge.Testing;
+
+namespace SupaCharge.UnitTests.Core.Config {
+  [TestFixture]
+  internal class ConfigWriterTests : BaseTestCase {
+    [Test]
+    public void TestSaveUnchangedConfigSavesSameXMLWithMultipleEntries() {
+      mFile.Setup(f => f.WriteAllText("config.xml", It.IsAny<string>()));
+      mWriter.Save();
+      mFile.Verify(f => f.WriteAllText("config.xml", It.Is<string>(s => XDocument
+                                                                          .Parse(s)
+                                                                          .XPathSelectElement("//configuration/appSettings/add")
+                                                                          .Attribute("value")
+                                                                          .Value == "joe")));
+    }
+
+    [Test]
+    public void TestSavingAChangedConfigChangesTheValuesOnASingleEntryConfig() {
+      mFile.Setup(f => f.WriteAllText("config.xml", It.IsAny<string>()));
+      mWriter.Set("value", "bob");
+      mWriter.Save();
+      mFile.Verify(f => f.WriteAllText("config.xml", It.Is<string>(s => XDocument
+                                                                          .Parse(s)
+                                                                          .XPathSelectElement("//configuration/appSettings/add")
+                                                                          .Attribute("value")
+                                                                          .Value == "bob")));
+    }
+
+    //var writer  =  new Writer(..., 'abc.xml')
+    //writer.set("aac_leave_on_exit", "true");
+    //writer.set("aac_ppg_portz", "1234");
+    //writer.Commit()
+    [SetUp]
+    public void DoSetup() {
+      mFile = Mok<IFile>();
+      mFile.Setup(f => f.ReadAllText("config.xml")).Returns(_SingleEntryXml);
+      mWriter = new ConfigWriter(mFile.Object, "config.xml");
+    }
+
+    private Mock<IFile> mFile;
+    private ConfigWriter mWriter;
+
+    private const string _SingleEntryXml =
+      @"<?xml version=""1.0"" encoding=""utf-8""?>
+<configuration>
+  <appSettings>
+    <add key=""name"" value=""joe"" />
+  </appSettings>
+</configuration>";
+    
+    private const string _MultiEntryXml =
+      @"<?xml version=""1.0"" encoding=""utf-8""?>
+<configuration>
+  <appSettings>
+    <add key=""name"" value=""joe"" />
+    <add key=""name"" value=""bob"" />
+    <add key=""name"" value=""hal"" />
+  </appSettings>
+</configuration>";
+  }
+}
