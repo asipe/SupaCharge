f7468805941c26fb0b4a4851f6fb162369a7d3c3
Andy Sipe <ajs.general@gmail.com>
2013-07-08 08:23:33 -0400

    updated build with net-3.5 specific build prefix

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -1,5 +1,5 @@
 thirdparty\nuget\NuGet.exe install src\SupaCharge.Nuget.Packages\common\packages.config -OutputDirectory packages\common
-thirdparty\nuget\NuGet.exe install src\SupaCharge.Nuget.Packages\3.5\packages.config -OutputDirectory packages\3.5
+thirdparty\nuget\NuGet.exe install src\SupaCharge.Nuget.Packages\net-3.5\packages.config -OutputDirectory packages\net-3.5
 
 $env:PATH += ";.\packages\common\NAnt.Portable.0.92\;.\packages\common\NUnit.Runners.2.6.1\tools"
 
