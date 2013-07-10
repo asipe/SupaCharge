b3ea5ea8d53ae93c759aebb6c5bfd60b4b9babae
Andy Sipe <ajs.general@gmail.com>
2013-07-10 07:31:27 -0400

    updated copyright dates and added lic to solution

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Z Bar Technologies, LLC")]
 [assembly: AssemblyProduct("SupaCharge.Core")]
-[assembly: AssemblyCopyright("(c) 2012 Morgan Sipe (morgan.talk@gmail.com), Andy Sipe (ajs.general@gmail.com)")]
+[assembly: AssemblyCopyright("(c) 2012-2013 Morgan Sipe (morgan.talk@gmail.com), Andy Sipe (ajs.general@gmail.com)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
