4b01cac759d4227007440627d14d10cf76e5542a
Andy Sipe <ajs.general@gmail.com>
2013-07-10 08:21:30 -0400

    added nuget support for SC testing

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("SupaCharge.Core")]
+[assembly: AssemblyTitle("SupaCharge")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Z Bar Technologies, LLC")]
-[assembly: AssemblyProduct("SupaCharge.Core")]
+[assembly: AssemblyProduct("SupaCharge")]
 [assembly: AssemblyCopyright("(c) 2012-2013 Morgan Sipe (morgan.talk@gmail.com), Andy Sipe (ajs.general@gmail.com)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("63965a07-5af7-4daa-9d8e-fc2f9db22842")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
