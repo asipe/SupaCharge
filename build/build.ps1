093ff6baf577c055177870843df183b88fec95ab
Andy Sipe <ajs.general@gmail.com>
2013-07-19 08:39:28 -0400

    additional wip on msbuild conversion

--- build/build.ps1
+++ build/build.ps1
@@ -8,16 +8,5 @@ function CheckLastExitCode() {
 
 powershell .\build\clean.ps1
 
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe .\build\SupaCharge.Core.proj /p:FrameworkVersion=v3.5
-CheckLastExitCode
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe .\build\SupaCharge.Testing.proj /p:FrameworkVersion=v3.5
-CheckLastExitCode
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe .\build\SupaCharge.UnitTests.proj /p:FrameworkVersion=v3.5
-CheckLastExitCode
-
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe .\build\SupaCharge.Core.proj /p:FrameworkVersion=v4.0
-CheckLastExitCode
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe .\build\SupaCharge.Testing.proj /p:FrameworkVersion=v4.0
-CheckLastExitCode
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe .\build\SupaCharge.UnitTests.proj /p:FrameworkVersion=v4.0
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe .\build\SupaCharge.proj /ds
 CheckLastExitCode
