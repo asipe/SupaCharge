434a2d09dcee4b2595016f7fdce8a09810f169fb
Andy Sipe <ajs.general@gmail.com>
2013-07-22 08:43:44 -0400

    additional work on converting to msbuild.  Added support for parallel builds and unit test execution

--- build/build.ps1
+++ build/build.ps1
@@ -6,7 +6,11 @@ function CheckLastExitCode() {
   }
 }
 
-powershell .\build\clean.ps1
+$times = Measure-Command {
+  powershell .\build\clean.ps1
 
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe .\build\SupaCharge.proj /ds
-CheckLastExitCode
+  C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe .\build\SupaCharge.proj /ds /maxcpucount:4 | Write-Host
+  CheckLastExitCode
+}
+
+Write-Host "Build Completed in $($times.TotalSeconds) seconds ($($times.TotalMilliseconds) millis)"
---
+++ build/runttests.ps1
@@ -0,0 +1,22 @@
+$ErrorActionPreference = 'Stop'
+
+function CheckLastExitCode() {
+  if ($? -eq $false) {
+    throw 'build failed'
+  }
+}
+
+
+Write-Host ''
+Write-Host '------------net-3.5------------' -ForegroundColor DarkYellow
+.\packages\common\NUnit.Runners\tools\nunit-console.exe debug\net-3.5\supacharge.unittests\supacharge.unittests.dll /nologo | Write-Host
+CheckLastExitCode
+Write-Host '-------------------------------' -ForegroundColor DarkYellow
+Write-Host ''
+
+Write-Host ''
+Write-Host '------------net-4.0------------' -ForegroundColor DarkYellow
+.\packages\common\NUnit.Runners\tools\nunit-console.exe debug\net-4.0\supacharge.unittests\supacharge.unittests.dll /nologo | Write-Host
+CheckLastExitCode
+Write-Host '-------------------------------' -ForegroundColor DarkYellow
+Write-Host ''
