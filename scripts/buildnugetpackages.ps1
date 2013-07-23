6e5266bd4f522bd61e223e82e0ff8a450741f4ae
Andy Sipe <ajs.general@gmail.com>
2013-07-23 08:00:36 -0400

    removed comments

--- scripts/buildnugetpackages.ps1
+++ scripts/buildnugetpackages.ps1
@@ -33,24 +33,3 @@ CheckLastExitCode
 
 thirdparty\nuget\nuget.exe pack .\nugetworking\testing\supacharge.testing.dll.nuspec -OutputDirectory .\nugetworking\testing | Write-Host
 CheckLastExitCode
-
-#    <delete dir="${nuget.working.dir}" verbose="${verbose}" />
-#    <copy todir="${nuget.working.dir}\core\lib\net35" file="${debug.dir}\net-3.5\SupaCharge.Core.dll" verbose="${verbose}"/>
-#    <copy todir="${nuget.working.dir}\core\lib\net40" file="${debug.dir}\net-4.0\SupaCharge.Core.dll" verbose="${verbose}"/>
-#    <copy todir="${nuget.working.dir}\core" file="${nuget.spec.dir}\SupaCharge.Core.dll.nuspec" verbose="${verbose}"/>
-#
-#    <exec program="thirdparty\nuget\nuget.exe"
-#          verbose="${verbose}"
-#          commandline="pack ${nuget.working.dir}\core\SupaCharge.Core.dll.nuspec -OutputDirectory ${nuget.working.dir}\core"
-#          workingdir="${root.dir}"
-#          failonerror="true"/>
-#
-#    <copy todir="${nuget.working.dir}\testing\lib\net35" file="${debug.dir}\net-3.5\SupaCharge.Testing.dll" verbose="${verbose}"/>
-#    <copy todir="${nuget.working.dir}\testing\lib\net40" file="${debug.dir}\net-4.0\SupaCharge.Testing.dll" verbose="${verbose}"/>
-#    <copy todir="${nuget.working.dir}\testing" file="${nuget.spec.dir}\SupaCharge.Testing.dll.nuspec" verbose="${verbose}"/>
-#
-#    <exec program="thirdparty\nuget\nuget.exe"
-#          verbose="${verbose}"
-#          commandline="pack ${nuget.working.dir}\testing\SupaCharge.Testing.dll.nuspec -OutputDirectory ${nuget.working.dir}\testing"
-#          workingdir="${root.dir}"
-#          failonerror="true"/>
