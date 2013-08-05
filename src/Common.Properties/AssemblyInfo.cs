d9797ab2e2c6adfe200507b191a52ff46d687fdd
Andy Sipe <ajs.general@gmail.com>
2013-08-05 07:48:20 -0400

    added project specific assembly infos

--- src/Common.Properties/AssemblyInfo.cs
+++ src/Common.Properties/AssemblyInfo.cs
@@ -1,8 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("SupaCharge")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Z Bar Technologies, LLC")]
 [assembly: AssemblyProduct("SupaCharge")]
@@ -10,6 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("63965a07-5af7-4daa-9d8e-fc2f9db22842")]
 [assembly: AssemblyVersion("1.0.0.3")]
 [assembly: AssemblyFileVersion("1.0.0.3")]
---
+++ src/SupaCharge.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SupaCharge.UnitTests")]
+[assembly: AssemblyDescription("SupaCharge.UnitTests")]
