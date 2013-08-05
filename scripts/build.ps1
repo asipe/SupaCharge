577a38959dfe8064160ccda0015f290dafdc86f7
Andy Sipe <ajs.general@gmail.com>
2013-08-05 07:02:52 -0400

    moved build scripts into src

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -9,7 +9,7 @@ function CheckLastExitCode() {
 $times = Measure-Command {
   powershell .\build\clean.ps1
 
-  C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe .\build\SupaCharge.proj /ds /maxcpucount:4 | Write-Host
+  C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe .\src\SupaCharge.Build\SupaCharge.proj /ds /maxcpucount:4 | Write-Host
   CheckLastExitCode
 }
 
