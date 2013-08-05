eb9c8e0b47a724ccf6392c9f31b6c0971a7c64ef
Andy Sipe <ajs.general@gmail.com>
2013-08-05 07:32:47 -0400

    added basic Console abstraction

---
+++ src/SupaCharge.Core/SystemAbstractions/DotNetConsole.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SupaCharge.Core.SystemAbstractions {
+  public class DotNetConsole : ISystemConsole {
+    public void WriteLine(string format, params object[] args) {
+      Console.WriteLine(format, args);
+    }
+
+    public void WriteLine(params string[] messages) {
+      Array.ForEach(messages, Console.WriteLine);
+    }
+
+    public void WriteLineColored(ConsoleColor color, string format, params object[] args) {
+      var currentColor = Console.ForegroundColor;
+      Console.ForegroundColor = color;
+      try {
+        WriteLine(format, args);
+      } finally {
+        Console.ForegroundColor = currentColor;
+      }
+    }
+
+    public void WriteLineColored(ConsoleColor color, params string[] messages) {
+      var currentColor = Console.ForegroundColor;
+      Console.ForegroundColor = color;
+      try {
+        WriteLine(messages);
+      } finally {
+        Console.ForegroundColor = currentColor;
+      }
+    }
+  }
+}
---
+++ src/SupaCharge.Core/SystemAbstractions/ISystemConsole.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SupaCharge.Core.SystemAbstractions {
+  public interface ISystemConsole {
+    void WriteLine(string format, params object[] args);
+    void WriteLine(params string[] messages);
+    void WriteLineColored(ConsoleColor color, string format, params object[] args);
+    void WriteLineColored(ConsoleColor color, params string[] messages);
+  }
+}
