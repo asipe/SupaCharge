aec5805bcfb6905b50a3f3d85322190d0d122ccb
Andy Sipe <ajs.general@gmail.com>
2013-08-09 08:44:23 -0400

    added Recording Event List

---
+++ src/SupaCharge.Core/DiagnosticAbstractions/RecordingEventList.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace SupaCharge.Core.DiagnosticAbstractions {
+  public class RecordingEventList {
+    public RecordingEventList(ITickCountProvider provider) {
+      mProvider = provider;
+      mStart = mProvider.GetTicks();
+      AddEvent(0, "Created");
+    }
+    
+    public void AddEvent(string msg) {
+      AddEvent(mProvider.GetTicks() - mStart, msg);
+    }
+
+    private void AddEvent(long ticks, string msg) {
+      msg = string.Format("{0}: {1}", ticks, msg);
+      lock (mLock) {
+        mEvents.Add(msg);
+      }
+    }
+
+    public string[] GetEvents() {
+      lock (mLock) {
+        return mEvents.ToArray();
+      }
+    }
+
+    private readonly List<string> mEvents = new List<string>();
+    private readonly object mLock = new object();
+    private readonly ITickCountProvider mProvider;
+    private readonly long mStart;
+  }
+}
---
+++ src/SupaCharge.UnitTests/Core/DiagnosticAbstractions/RecordingEventListTest.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using NUnit.Framework;
+using SupaCharge.Core.DiagnosticAbstractions;
+using SupaCharge.Testing;
+
+namespace SupaCharge.UnitTests.Core.DiagnosticAbstractions {
+  [TestFixture]
+  public class RecordingEventListTest : BaseTestCase {
+    [Test]
+    public void TestDefaultEventListHasInitialEntry() {
+      Assert.That(mList.GetEvents(), Is.EqualTo(BA("0: Created")));
+    }
+
+    [Test]
+    public void TestAddingEvents() {
+      mProvider.Setup(p => p.GetTicks()).Returns(1500);
+      mList.AddEvent("event1");
+      Assert.That(mList.GetEvents(), Is.EqualTo(BA("0: Created", "500: event1")));
+      mProvider.Setup(p => p.GetTicks()).Returns(1750);
+      mList.AddEvent("event2");
+      Assert.That(mList.GetEvents(), Is.EqualTo(BA("0: Created", "500: event1", "750: event2")));
+    }
+
+    [SetUp]
+    public void DoSetup() {
+      mProvider = Mok<ITickCountProvider>();
+      mProvider.Setup(p => p.GetTicks()).Returns(1000);
+      mList = new RecordingEventList(mProvider.Object);
+    }
+
+    private RecordingEventList mList;
+    private Mock<ITickCountProvider> mProvider;
+  }
+}
