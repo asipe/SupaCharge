3f1ad42d49d1d2897d929e435e08c92ee92619f9
Andy Sipe <ajs.general@gmail.com>
2013-08-23 14:59:04 -0400

    added WriteAllLines to IFile interface

--- src/SupaCharge.Core/IOAbstractions/IFile.cs
+++ src/SupaCharge.Core/IOAbstractions/IFile.cs
@@ -9,6 +9,7 @@ namespace SupaCharge.Core.IOAbstractions {
     byte[] ReadAllBytes(string path);
     void WriteAllBytes(string path, byte[] bytes);
     void WriteAllText(string path, string text);
+    void WriteAllLines(string path, params string[] contents);
     void Copy(string sourceFileName, string destFileName);
     bool Exists(string path);
     void Delete(string path);
