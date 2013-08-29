101fba0fa9bb9ef6bc90b19306e1f3634d270ce6
Andy Sipe <ajs.general@gmail.com>
2013-08-29 08:59:40 -0400

    added synchronous work queue impl

---
+++ src/SupaCharge.Core/ThreadingAbstractions/SynchronousWorkQueue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace SupaCharge.Core.ThreadingAbstractions {
+  public class SynchronousWorkQueue : IWorkQueue {
+    public EmptyFuture Enqueue(Action work) {
+      var future = new EmptyFuture();
+      DoWork(work, future);
+      return future;
+    }
+
+    public EmptyFuture Enqueue<T>(T data, Action<T> work) {
+      var future = new EmptyFuture();
+      DoWork(data, work, future);
+      return future;
+    }
+
+    public ResultFuture<T> Enqueue<T>(Func<T> work) {
+      var future = new ResultFuture<T>();
+      DoWork(work, future);
+      return future;
+    }
+
+    public ResultFuture<TResult> Enqueue<T, TResult>(T data, Func<T, TResult> work) {
+      var future = new ResultFuture<TResult>();
+      DoWork(data, work, future);
+      return future;
+    }
+
+    private static void DoWork<T, TResult>(T data, Func<T, TResult> work, ResultFuture<TResult> future) {
+      try {
+        future.Set(work(data));
+      } catch (Exception e) {
+        future.Failed(e);
+      }
+    }
+
+    private static void DoWork<T>(Func<T> work, ResultFuture<T> future) {
+      try {
+        future.Set(work());
+      } catch (Exception e) {
+        future.Failed(e);
+      }
+    }
+
+    private static void DoWork(Action work, EmptyFuture future) {
+      try {
+        work();
+        future.Set();
+      } catch (Exception e) {
+        future.Failed(e);
+      }
+    }
+
+    private static void DoWork<T>(T data, Action<T> work, EmptyFuture future) {
+      try {
+        work(data);
+        future.Set();
+      } catch (Exception e) {
+        future.Failed(e);
+      }
+    }
+  }
+}
