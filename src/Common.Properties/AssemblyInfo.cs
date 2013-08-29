d05c596e4dfc5895f15d9709391301b44a682b63
Andy Sipe <ajs.general@gmail.com>
2013-08-29 11:40:35 -0400

    rev'd to version 1.0.0.9

--- src/Common.Properties/AssemblyInfo.cs
+++ src/Common.Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.8")]
-[assembly: AssemblyFileVersion("1.0.0.8")]
+[assembly: AssemblyVersion("1.0.0.9")]
+[assembly: AssemblyFileVersion("1.0.0.9")]
