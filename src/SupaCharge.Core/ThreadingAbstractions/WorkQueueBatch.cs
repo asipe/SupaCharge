343944878e32bddd7358f87debc8976e59dc3462
Andy Sipe <ajs.general@gmail.com>
2013-08-29 11:21:51 -0400

    added work queue batch

---
+++ src/SupaCharge.Core/ThreadingAbstractions/WorkQueueBatch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SupaCharge.Core.ThreadingAbstractions {
+  public class WorkQueueBatch {
+    public WorkQueueBatch(IWorkQueue queue) {
+      mQueue = queue;
+    }
+
+    public WorkQueueBatch Add(params Action[] work) {
+      mFutures.AddRange(work.Select(w => mQueue.Enqueue(w)).ToArray());
+      return this;
+    }
+
+    public WorkQueueBatch Add<T>(T data, Action<T> work) {
+      mFutures.Add(mQueue.Enqueue(data, work));
+      return this;
+    }
+
+    public WorkQueueBatch Wait(int millisecondsTimeout) {
+      Array.ForEach(mFutures.ToArray(), future => future.Wait(millisecondsTimeout));
+      return this;
+    }
+
+    private readonly List<EmptyFuture> mFutures = new List<EmptyFuture>();
+    private readonly IWorkQueue mQueue;
+  }
+}
---
+++ src/SupaCharge.UnitTests/Core/ThreadingAbstractions/WorkQueueBatchTest.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using SupaCharge.Core.ThreadingAbstractions;
+using SupaCharge.Testing;
+
+namespace SupaCharge.UnitTests.Core.ThreadingAbstractions {
+  [TestFixture]
+  public class WorkQueueBatchTest : BaseTestCase {
+    [Test]
+    public void TestWaitWithNoItems() {
+      mBatch.Wait(0);
+    }
+
+    [Test]
+    public void TestAddSingleItemWithWait() {
+      mBatch
+        .Add(Inc)
+        .Wait(0);
+      Assert.That(mCount, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void TestAddMultipleItemsWithWait() {
+      mBatch
+        .Add(Inc)
+        .Add(Inc)
+        .Add(Inc, Inc)
+        .Wait(0);
+      Assert.That(mCount, Is.EqualTo(4));
+    }
+
+    [Test]
+    public void TestAddMultipleItemsWithDataWithWait() {
+      mBatch
+        .Add(15, Add)
+        .Add(50, Add)
+        .Add(100, Add)
+        .Wait(0);
+      Assert.That(mCount, Is.EqualTo(165));
+    }
+
+    [Test]
+    public void TestAddMixedItemsWithDataAndWithOutDataWithWait() {
+      mBatch
+        .Add(100, Add)
+        .Add(Inc, Inc, Inc)
+        .Wait(0)
+        .Add(200, Add)
+        .Add(Inc)
+        .Wait(0);
+      Assert.That(mCount, Is.EqualTo(304));
+    }
+
+    [SetUp]
+    public void DoSetup() {
+      mQueue = new SynchronousWorkQueue();
+      mBatch = new WorkQueueBatch(mQueue);
+      mCount = 0;
+    }
+
+    private void Inc() {
+      ++mCount;
+    }
+
+    private void Add(int amt) {
+      mCount += amt;
+    }
+
+    private SynchronousWorkQueue mQueue;
+    private WorkQueueBatch mBatch;
+    private int mCount;
+  }
+}
