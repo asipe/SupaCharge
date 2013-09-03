0c805fd05f55a905b23a515007b379a5d21827e2
Andy Sipe <ajs.general@gmail.com>
2013-09-03 08:05:10 -0400

    added simple Args class to help wrap multiple params

---
+++ src/SupaCharge.Core/Collections/Args.cs
@@ -0,0 +1,13 @@
+﻿namespace SupaCharge.Core.Collections {
+  public class Args {
+    public Args(params object[] args) {
+      mArgs = args;
+    }
+
+    public T Get<T>(int index) {
+      return (T)mArgs[index];
+    }
+
+    private readonly object[] mArgs;
+  }
+}
---
+++ src/SupaCharge.UnitTests/Core/Collections/ArgsTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using SupaCharge.Core.Collections;
+using SupaCharge.Testing;
+
+namespace SupaCharge.UnitTests.Core.Collections {
+  [TestFixture]
+  public class ArgsTest : BaseTestCase {
+    [Test]
+    public void TestUsge() {
+      var args = new Args(1, "abc", new DateTime(2013, 10, 15), this);
+      Assert.That(args.Get<int>(0), Is.EqualTo(1));
+      Assert.That(args.Get<string>(1), Is.EqualTo("abc"));
+      Assert.That(args.Get<DateTime>(2), Is.EqualTo(new DateTime(2013, 10, 15)));
+      Assert.That(args.Get<ArgsTest>(3), Is.SameAs(this));
+    }
+  }
+}
