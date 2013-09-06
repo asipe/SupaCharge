c94e63f10ddf17567762d1fde198c8316d2bbc30
Andy Sipe <ajs.general@gmail.com>
2013-09-06 08:23:52 -0400

    added Retry impl

---
+++ src/SupaCharge.Core/ThreadingAbstractions/Retry.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace SupaCharge.Core.ThreadingAbstractions {
+  public class Retry {
+    public Retry(int numberOfAttempts, int millisBetweenAttempts) {
+      mNumberOfAttempts = numberOfAttempts;
+      mMillisBetweenAttempts = millisBetweenAttempts;
+    }
+
+    public Retry WithWork(Action<int> work) {
+      mWork = work;
+      return this;
+    }
+
+    public Retry WithErrorHandler(Action<Exception> handler) {
+      mErrorHandler = handler;
+      return this;
+    }
+
+    public void Start() {
+      var iteration = 0;
+      while (true)
+        try {
+          mWork(iteration++);
+          break;
+        } catch (Exception ex) {
+          if (iteration >= mNumberOfAttempts) {
+            mErrorHandler(ex);
+            break;
+          }
+          Thread.Sleep(mMillisBetweenAttempts);
+        }
+    }
+
+    private readonly int mMillisBetweenAttempts;
+    private readonly int mNumberOfAttempts;
+    private Action<Exception> mErrorHandler = ex => {throw ex;};
+    private Action<int> mWork;
+  }
+}
---
+++ src/SupaCharge.UnitTests/Core/ThreadingAbstractions/RetryTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
+using SupaCharge.Core.ThreadingAbstractions;
+using SupaCharge.Testing;
+
+namespace SupaCharge.UnitTests.Core.ThreadingAbstractions {
+  [TestFixture]
+  public class RetryTest : BaseTestCase {
+    private class WorkInfo {
+      public int Iteration{get;set;}
+      public long Millis{get;set;}
+    }
+
+    [Test]
+    public void TestStartCallsWorkOnceAndExitsIfSuccess() {
+      new Retry(10, 100)
+        .WithWork(DoWork)
+        .Start();
+
+      Assert.That(mList.Count, Is.EqualTo(1));
+      Assert.That(mList[0].Iteration, Is.EqualTo(0));
+      Assert.That(mList[0].Millis, Is.LessThan(25));
+    }
+
+    [Test]
+    public void TestStartCallsWorkUntilThereIsSuccess() {
+      mThrowUntil = 5;
+
+      new Retry(10, 50)
+        .WithWork(DoWork)
+        .Start();
+
+      Assert.That(mList.Count, Is.EqualTo(6));
+      Assert.That(mList.Select(i => i.Iteration), Is.EqualTo(BA(0, 1, 2, 3, 4, 5)));
+      Assert.That(mList[0].Millis, Is.LessThan(25));
+      Assert.That(mList.Skip(1).Select(i => i.Millis), Is.All.GreaterThan(40).And.All.LessThan(100));
+    }
+
+    [Test]
+    public void TestStartThrowsWhenThereIsNoSuccessAndAllIterationsAreExpended() {
+      mThrowUntil = int.MaxValue;
+
+      var ex = Assert.Throws<Exception>(() => new Retry(5, 50).WithWork(DoWork).Start());
+      Assert.That(ex.Message, Is.EqualTo("throwing on iteration: 4"));
+
+      Assert.That(mList.Count, Is.EqualTo(5));
+      Assert.That(mList.Select(i => i.Iteration), Is.EqualTo(BA(0, 1, 2, 3, 4)));
+      Assert.That(mList[0].Millis, Is.LessThan(25));
+      Assert.That(mList.Skip(1).Select(i => i.Millis), Is.All.GreaterThan(40).And.All.LessThan(100));
+    }
+
+    [Test]
+    public void TestStartCallsErrorHandlerWhenThereIsNoSuccessAndErrorHandlerIsDefined() {
+      mThrowUntil = int.MaxValue;
+      Exception lastException = null;
+
+      new Retry(5, 50)
+        .WithWork(DoWork)
+        .WithErrorHandler(ex => lastException = ex)
+        .Start();
+
+      Assert.That(lastException.Message, Is.EqualTo("throwing on iteration: 4"));
+      Assert.That(mList.Count, Is.EqualTo(5));
+      Assert.That(mList.Select(i => i.Iteration), Is.EqualTo(BA(0, 1, 2, 3, 4)));
+      Assert.That(mList[0].Millis, Is.LessThan(25));
+      Assert.That(mList.Skip(1).Select(i => i.Millis), Is.All.GreaterThan(40).And.All.LessThan(100));
+    }
+
+    [SetUp]
+    public void DoSetup() {
+      mWatch = Stopwatch.StartNew();
+      mList = new List<WorkInfo>();
+      mThrowUntil = -1;
+    }
+
+    private Stopwatch mWatch;
+    private List<WorkInfo> mList;
+    private int mThrowUntil;
+
+    private void DoWork(int x) {
+      mList.Add(new WorkInfo {Iteration = x, Millis = mWatch.ElapsedMilliseconds});
+      mWatch.Reset();
+      mWatch.Start();
+
+      if (mThrowUntil == -1)
+        return;
+
+      if (x != mThrowUntil)
+        throw new Exception("throwing on iteration: " + x);
+    }
+  }
+}
