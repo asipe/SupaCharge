ce30ea36aa33bba9d898b6a1c55be194cc3f4dce
msipe <morgan.talk@gmail.com>
2013-10-01 15:34:12 -0400

    Added the file montoring tests + file

---
+++ src/SupaCharge.Core/Monitoring/DirMonitor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace SupaCharge.Core.Monitoring {
+  public class DirMonitor {
+    public DirMonitor(string pathToWatch) {
+      mWatcher = new FileSystemWatcher(pathToWatch);
+    }
+
+    public void Start() {
+      mWatcher.EnableRaisingEvents = true;
+    }
+
+    public void SetChangedFunctionality() {
+      mWatcher.Changed += new FileSystemEventHandler(OnFileChange);
+    }
+
+    public void Stop() {
+      mWatcher.EnableRaisingEvents = false;
+    }
+
+    public event EventHandler<FileSystemEventArgs> OnFileChange;
+    private readonly FileSystemWatcher mWatcher;
+  }
+}
---
+++ src/SupaCharge.UnitTests/Core/Monitoring/FileMonitoringTest.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using NUnit.Framework;
+using SupaCharge.Core.Monitoring;
+using SupaCharge.Core.ThreadingAbstractions;
+using SupaCharge.Testing;
+
+namespace SupaCharge.UnitTests.Core.Monitoring {
+  [TestFixture]
+  public class FileMonitoringTest : BaseTestCase {
+    [Test]
+    public void TestChangesToAFileResultInChangeEventBeingThrown() {
+      var seen = new List<string>();
+      mMonitor.OnFileChange += (o, a) => seen.Add(a.Name);
+      mMonitor.SetChangedFunctionality();
+      mMonitor.Start();
+
+      using (var strm = File.OpenWrite(mFile1Path)) {
+        var buf = Encoding.ASCII.GetBytes("Hello");
+        strm.Write(buf, 0, buf.Length);
+        strm.Close();
+      }
+
+      new Retry(100, 50)
+        .WithWork(m => Assert.That(seen, Is.EqualTo(BA("file1.txt"))))
+        .Start();
+
+      mMonitor.Stop();
+    }
+
+    [SetUp]
+    public void DoSetup() {
+      CreateTempDir();
+      mFile1Path = Path.Combine(TempDir, "file1.txt");
+      File.WriteAllText(mFile1Path, "file.txt");
+      mMonitor = new DirMonitor(TempDir);
+    }
+
+    private string mFile1Path;
+    private DirMonitor mMonitor;
+  }
+}
