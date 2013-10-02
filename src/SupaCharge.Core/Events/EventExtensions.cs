d4ece6a2048c0fbc6390577ac3e0c0f6de2d9ce9
msipe <morgan.talk@gmail.com>
2013-10-02 09:53:08 -0400

    Added the raise event tc's + class

---
+++ src/SupaCharge.Core/Events/EventExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SupaCharge.Core.Events {
+  public static class EventExtensions {
+    public static void RaiseEvent<T>(this EventHandler<T> evt, object sender, T args) where T : EventArgs {
+      if (evt != null)
+        evt(sender, args);
+    }
+  }
+}
