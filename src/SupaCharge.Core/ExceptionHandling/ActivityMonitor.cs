b3824913b1ba94861bb19d1534c649c5d50d60bd
Andy Sipe <ajs.general@gmail.com>
2013-10-15 07:54:31 -0400

    added ActivityMonitor

---
+++ src/SupaCharge.Core/ExceptionHandling/ActivityMonitor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SupaCharge.Core.ExceptionHandling {
+  public class ActivityMonitor {
+    public void Resolve() {
+      if (mErrors.Any())
+        throw new AggregatedException(mErrors.ToArray(), "{0} Activities Failed", mErrors.Count());
+    }
+
+    public void Monitor(Action activity) {
+      try {
+        activity();
+      } catch (Exception e) {
+        mErrors.Add(e);
+      }
+    }
+
+    private readonly List<Exception> mErrors = new List<Exception>();
+  }
+}
---
+++ src/SupaCharge.UnitTests/Core/ExceptionHandling/ActivityMonitorTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using NUnit.Framework;
+using SupaCharge.Core.ExceptionHandling;
+using SupaCharge.Testing;
+
+namespace SupaCharge.UnitTests.Core.ExceptionHandling {
+  [TestFixture]
+  public class ActivityMonitorTest : BaseTestCase {
+    [Test]
+    public void TestResolveDoesNotThrowByDefault() {
+      mList.Resolve();
+    }
+
+    [Test]
+    public void TestResolveDoesNotThrowIfSingleActivitySucceeded() {
+      var count = 0;
+      mList.Monitor(() => ++count);
+      mList.Resolve();
+      Assert.That(count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void TestResolveDoesNotThrowIfMultipleActivitiesSucceed() {
+      var count = 0;
+      mList.Monitor(() => ++count);
+      mList.Monitor(() => ++count);
+      mList.Monitor(() => ++count);
+      mList.Resolve();
+      Assert.That(count, Is.EqualTo(3));
+    }
+
+    [Test]
+    public void TestResolveThrowsIfSingleActivityFailed() {
+      mList.Monitor(() => {throw new Exception("test error");});
+      var ex = Assert.Throws<AggregatedException>(() => mList.Resolve());
+      Assert.That(ex.Message, Is.EqualTo("1 Activities Failed"));
+      Assert.That(ex.ToString(), Is.StringContaining("test error"));
+    }
+
+    [Test]
+    public void TestResolveThrowsIfMultipleActivitiesFailed() {
+      mList.Monitor(() => {throw new Exception("test error 1");});
+      mList.Monitor(() => {throw new Exception("test error 2");});
+      mList.Monitor(() => {throw new Exception("test error 3");});
+      var ex = Assert.Throws<AggregatedException>(() => mList.Resolve());
+      Assert.That(ex.Message, Is.EqualTo("3 Activities Failed"));
+      Assert.That(ex.ToString(), Is
+                                   .StringContaining("test error 1")
+                                   .And
+                                   .StringContaining("test error 2")
+                                   .And
+                                   .StringContaining("test error 3"));
+    }
+
+    [SetUp]
+    public void DoSetup() {
+      mList = new ActivityMonitor();
+    }
+
+    private ActivityMonitor mList;
+  }
+}
