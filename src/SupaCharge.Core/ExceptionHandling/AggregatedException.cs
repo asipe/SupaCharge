a9b871074c30bc1da79f3e82b329ea40a8c63231
Andy Sipe <ajs.general@gmail.com>
2013-10-15 07:33:25 -0400

    added aggregated exception

---
+++ src/SupaCharge.Core/ExceptionHandling/AggregatedException.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace SupaCharge.Core.ExceptionHandling {
+  [Serializable]
+  public class AggregatedException : Exception {
+    public AggregatedException(Exception[] exceptions) {
+      Exceptions = exceptions;
+    }
+
+    public AggregatedException(Exception[] exceptions, SerializationInfo info, StreamingContext context) : base(info, context) {
+      Exceptions = exceptions;
+    }
+
+    public AggregatedException(Exception[] exceptions, string msg) : base(msg) {
+      Exceptions = exceptions;
+    }
+
+    public AggregatedException(Exception[] exceptions, string msg, Exception e) : base(msg, e) {
+      Exceptions = exceptions;
+    }
+
+    public AggregatedException(Exception[] exceptions, string msg, params object[] fmt) : base(string.Format(msg, fmt)) {
+      Exceptions = exceptions;
+    }
+
+    public Exception[] Exceptions{get;private set;}
+
+    public override string ToString() {
+      var result = new StringBuilder();
+      result.AppendLine(base.ToString());
+
+      foreach (var exception in Exceptions) {
+        result.AppendLine();
+        result.AppendLine();
+        result.AppendLine(exception.ToString());
+      }
+
+      return result.ToString();
+    }
+  }
+}
---
+++ src/SupaCharge.UnitTests/Core/ExceptionHandling/AggregatedExceptionTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using SupaCharge.Core.ExceptionHandling;
+using SupaCharge.Testing;
+
+namespace SupaCharge.UnitTests.Core.ExceptionHandling {
+  [TestFixture]
+  public class AggregatedExceptionTest : BaseTestCase {
+    [Test]
+    public void TestToString() {
+      var ex = new AggregatedException(BA(GetException("one"), GetException("two")));
+      Assert.That(ex.ToString(), Is
+                                   .StringContaining("one")
+                                   .And
+                                   .StringContaining("two"));
+    }
+
+    private static Exception GetException(string msg) {
+      try {
+        ThrowException(msg);
+      } catch (Exception e) {
+        return e;
+      }
+      return null;
+    }
+
+    private static void ThrowException(string msg) {
+      throw new Exception(msg);
+    }
+  }
+}
