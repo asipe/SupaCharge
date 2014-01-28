3b4851c1f7c97f04155d8e54ceb9e67482ba5743
Andy Sipe <ajs.general@gmail.com>
2014-01-28 15:15:35 -0500

    added GetMonthStartDate extension

---
+++ src/SupaCharge.Core/DateAndTime/Extensions/GetMonthStartExtension.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SupaCharge.Core.DateAndTime.Extensions {
+  public static class GetMonthStartExtension {
+    public static DateTime GetMonthStartDate(this DateTime dt) {
+      return new DateTime(dt.Year, dt.Month, 1);
+    }
+  }
+}
