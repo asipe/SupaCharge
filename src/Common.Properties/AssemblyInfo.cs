b52e6228c2c67dbef301471bfaf7392c391cc5a6
Andy Sipe <ajs.general@gmail.com>
2014-06-06 07:23:14 -0400

    udpated to 1.0.0.22

--- src/Common.Properties/AssemblyInfo.cs
+++ src/Common.Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.21")]
-[assembly: AssemblyFileVersion("1.0.0.21")]
+[assembly: AssemblyVersion("1.0.0.22")]
+[assembly: AssemblyFileVersion("1.0.0.22")]
