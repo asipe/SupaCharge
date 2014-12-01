2b49cbc4844ad575b515e745a9fbd09d47b848c6
Andy Sipe <ajs.general@gmail.com>
2014-12-01 06:39:33 -0500

    updated so frequency is recorded

--- src/SupaCharge.Core/DiagnosticAbstractions/RecordingEventList.cs
+++ src/SupaCharge.Core/DiagnosticAbstractions/RecordingEventList.cs
@@ -4,12 +4,14 @@ namespace SupaCharge.Core.DiagnosticAbstractions {
   public class RecordingEventList : IRecordingEventList {
     public RecordingEventList(ITickCountProvider provider) {
       mProvider = provider;
+      AddMessage(string.Format("Frequency: {0}", mProvider.GetFrequency()));
+      AddMessage("----------");
+      AddStampedEvent(0, "Created");
       mStart = mProvider.GetTicks();
-      AddEvent(0, "Created");
     }
 
     public void AddEvent(string msg, params object[] args) {
-      AddEvent(mProvider.GetTicks() - mStart, msg, args);
+      AddStampedEvent(mProvider.GetTicks() - mStart, msg, args);
     }
 
     public string[] GetEvents() {
@@ -18,8 +20,11 @@ namespace SupaCharge.Core.DiagnosticAbstractions {
       }
     }
 
-    private void AddEvent(long ticks, string msg, params object[] args) {
-      msg = string.Format("{0}: {1}", ticks, string.Format(msg, args));
+    private void AddStampedEvent(long ticks, string msg, params object[] args) {
+      AddMessage(string.Format("{0}: {1}", ticks, string.Format(msg, args)));
+    }
+
+    private void AddMessage(string msg) {
       lock (mLock) {
         mEvents.Add(msg);
       }
--- src/SupaCharge.UnitTests/Core/DiagnosticAbstractions/RecordingEventListTest.cs
+++ src/SupaCharge.UnitTests/Core/DiagnosticAbstractions/RecordingEventListTest.cs
@@ -8,22 +8,23 @@ namespace SupaCharge.UnitTests.Core.DiagnosticAbstractions {
   public class RecordingEventListTest : BaseTestCase {
     [Test]
     public void TestDefaultEventListHasInitialEntry() {
-      Assert.That(mList.GetEvents(), Is.EqualTo(BA("0: Created")));
+      Assert.That(mList.GetEvents(), Is.EqualTo(BA("Frequency: 1", "----------", "0: Created")));
     }
 
     [Test]
     public void TestAddingEvents() {
       mProvider.Setup(p => p.GetTicks()).Returns(1500);
       mList.AddEvent("event1");
-      Assert.That(mList.GetEvents(), Is.EqualTo(BA("0: Created", "500: event1")));
+      Assert.That(mList.GetEvents(), Is.EqualTo(BA("Frequency: 1", "----------", "0: Created", "500: event1")));
       mProvider.Setup(p => p.GetTicks()).Returns(1750);
       mList.AddEvent("event2 {0} {1}", "a", 1);
-      Assert.That(mList.GetEvents(), Is.EqualTo(BA("0: Created", "500: event1", "750: event2 a 1")));
+      Assert.That(mList.GetEvents(), Is.EqualTo(BA("Frequency: 1", "----------", "0: Created", "500: event1", "750: event2 a 1")));
     }
 
     [SetUp]
     public void DoSetup() {
       mProvider = Mok<ITickCountProvider>();
+      mProvider.Setup(p => p.GetFrequency()).Returns(1);
       mProvider.Setup(p => p.GetTicks()).Returns(1000);
       mList = new RecordingEventList(mProvider.Object);
     }
