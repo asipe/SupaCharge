2301d4efb5d74d7b8ade3f37446403d88774f145
Andy Sipe <ajs.general@gmail.com>
2015-01-23 07:59:32 -0500

    updated copyright dates

--- src/SupaCharge.Properties/AssemblyInfo.cs
+++ src/SupaCharge.Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Z Bar Technologies, LLC")]
 [assembly: AssemblyProduct("SupaCharge")]
-[assembly: AssemblyCopyright("(c) 2012-2013 Morgan Sipe (morgan.talk@gmail.com), Andy Sipe (ajs.general@gmail.com)")]
+[assembly: AssemblyCopyright("(c) 2012-2015 Morgan Sipe (morgan.talk@gmail.com), Andy Sipe (ajs.general@gmail.com)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
