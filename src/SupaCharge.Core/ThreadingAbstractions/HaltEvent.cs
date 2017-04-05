ea5ae74b9ec2d1366e820993e1ea3b146fb7123c
Gustav Rodewald <gustav.rodewald@eqis.com>
2017-04-05 10:21:55 -0500

    Added HaltEvent, updated Retry to be interruptable, updated unit tests

---
+++ src/SupaCharge.Core/ThreadingAbstractions/HaltEvent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace SupaCharge.Core.ThreadingAbstractions {
+  public class HaltEvent : IHaltEvent, IDisposable {
+    public HaltEvent() {
+      mEvent = new ManualResetEvent(false);
+    }
+
+    public void Dispose() {
+      Dispose(true);
+      GC.SuppressFinalize(this);
+    }
+
+    public void Set() {
+      mEvent.Set();
+    }
+
+    public void Reset() {
+      mEvent.Reset();
+    }
+
+    public bool IsSet() {
+      return mEvent.WaitOne(0);
+    }
+
+    protected virtual void Dispose(bool disposing) {
+      if (!disposing)
+        return;
+      if (mEvent == null)
+        return;
+      mEvent.Close();
+      mEvent = null;
+    }
+
+    private ManualResetEvent mEvent;
+  }
+}
---
+++ src/SupaCharge.UnitTests/Core/ThreadingAbstractions/HaltEventTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using SupaCharge.Core.ThreadingAbstractions;
+using SupaCharge.Testing;
+
+namespace SupaCharge.UnitTests.Core.ThreadingAbstractions {
+  [TestFixture]
+  public class HaltEventTest : BaseTestCase {
+    [Test]
+    public void TestConstructorInitializesToFalse() {
+      Assert.That(mHalt.IsSet(), Is.False);
+    }
+
+    [Test]
+    public void TestSetChangesIsSetToTrue() {
+      mHalt.Set();
+      Assert.That(mHalt.IsSet(), Is.True);
+    }
+
+    [Test]
+    public void TestResetChangesIsSetToFalse() {
+      mHalt.Set();
+      mHalt.Reset();
+      Assert.That(mHalt.IsSet(), Is.False);
+    }
+
+    [SetUp]
+    public void SetUp() {
+      mHalt = new HaltEvent();
+    }
+
+    private HaltEvent mHalt;
+  }
+}
